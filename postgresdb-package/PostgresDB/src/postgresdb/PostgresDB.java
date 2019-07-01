a5030ef28abd21a627b57fcc97a69d7a7996ff27
Ujjwal Raizada <ujjwalraizada@gmail.com>
2019-07-01 13:50:32 +0530

    postgresdb: Add abstract functions for database interfacing.

--- postgresdb-package/PostgresDB/src/postgresdb/PostgresDB.java
+++ postgresdb-package/PostgresDB/src/postgresdb/PostgresDB.java
@@ -1,9 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package postgresdb;
+
+import java.util.HashMap; 
+import java.util.Map; 
 
 import java.sql.*;
 
@@ -25,11 +23,32 @@ public class PostgresDB {
     
     public static void main(String[] args) {
         // TODO code application logic here
+         InitializeDatabase();
+        testInsertRecord();
+        
+        
+    }
+    
+    public static void testInsertRecord() {
+    
+        HashMap<String, String> record = new HashMap<>();
+        record.put("district", "d");
+        record.put("tehsil", "t");
+        record.put("village", "v");
+        record.put("share_added", "sa");
+        record.put("share_removed", "sr");
+        record.put("mall_guzari", "mg");
+        record.put("order_number", "on");
+        record.put("date", "date");
+        record.put("remark", "remark");
+        
+        insertRecord(record);
+    
     }
     
     public static String databaseURL() {
     
-        String URL = String.format("jdbc:postgres://%s:%s/%s", db_url, db_port, db_name);
+        String URL = String.format("jdbc:postgresql://%s:%s/%s", db_url, db_port, db_name);
         return URL;  
     }
     
@@ -55,26 +74,72 @@ public class PostgresDB {
         
         try {
         
-            int affectedRows = pstmt.executeUpdate();
-            
-            // check the affected rows 
-            if (affectedRows > 0) {
-                // get the ID back
-                try (ResultSet rs = pstmt.getGeneratedKeys()) {
-                    if (rs.next()) {
-                        id = rs.getLong(1);
-                    }
-                } catch (SQLException ex) {
-                    System.out.println(ex.getMessage());
-                }
-            }
-            
+            ResultSet rs = pstmt.executeQuery();
+            rs.next();
+            id = rs.getInt(1);
+  
         }
         catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
         
         return id;
+    }
+    
+    public static void insertRecord(HashMap<String, String> recordMap) {
+    
+        /*
+        
+        District
+        Tehesil
+        Village
+        
+        Share_Added
+        Share_Removed
+        Mall_Guzari
+        order_number
+        date
+        Remark
+        
+        */
+        
+        String district = recordMap.get("district");
+        String tehsil = recordMap.get("tehsil");
+        String village = recordMap.get("village");
+        String share_added = recordMap.get("share_added");
+        String share_removed = recordMap.get("share_removed");
+        String mall_guzari = recordMap.get("mall_guzari");
+        String order_number = recordMap.get("order_number");
+        String date = recordMap.get("date");
+        String remark = recordMap.get("remark");
+        
+        Connection conn = DatabaseConnect();
+        String SQL = "INSERT INTO register6 (district, tehsil, village, share_added, share_removed, mall_guzari, order_number, date, remark)" +
+                "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?) RETURNING sno;";
+        
+        try {
+        
+            PreparedStatement pstmt = conn.prepareStatement(SQL);
+            
+            pstmt.setString(1, district);
+            pstmt.setString(2, tehsil);
+            pstmt.setString(3, village);
+            pstmt.setString(4, share_added);
+            pstmt.setString(5, share_removed);
+            pstmt.setString(6, mall_guzari);
+            pstmt.setString(7, order_number);
+            pstmt.setString(8, date);
+            pstmt.setString(9, remark);
+            
+            System.out.println(insertData(pstmt));
+            
+        }
+        catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        
+        
     }
     
     public static ResultSet retrieveData(PreparedStatement pstmt) {
@@ -94,16 +159,40 @@ public class PostgresDB {
     
     public static void InitializeDatabase() {
         
+        /*
+        
+        District
+        Tehesil
+        Village
+        
+        Share_Added
+        Share_Removed
+        Mall_Guzari
+        order_number
+        date
+        Remark
+        
+        */
+        
+        
         Connection conn = DatabaseConnect();
-        String SQL = "CREATE TABLE DEMO (" +
-                "Name varchar(30)," +
-                "ID varchar(30) PRIMARY KEY" +
+        String SQL = "CREATE TABLE register6 (" +
+                "sno SERIAL," +
+                "District varchar(100)," +
+                "Tehsil varchar(100)," +
+                "Village varchar(100)," +
+                "share_Added varchar(100)," +
+                "share_removed varchar(100)," +
+                "mall_guzari varchar(100)," +
+                "order_number varchar(100)," +
+                "date varchar(100)," +
+                "remark varchar(100)" +
                 ");";
         
         try {
         
             PreparedStatement pstmt = conn.prepareStatement(SQL);
-            insertData(pstmt);
+            System.out.println(insertData(pstmt));
             
         }
         catch (SQLException ex) {
